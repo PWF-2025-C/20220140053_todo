110e298e152a112502ab703b40d125e7eed095fd
andiniseptia <andiniseptiandri124@gmail.com>
2025-03-15 16:22:14 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function todo()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
