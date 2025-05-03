5ec26d72d88b86857d104e016a390eae3abfec09
andiniseptia <andiniseptiandri124@gmail.com>
2025-05-03 15:23:52 +0700

    middleware, gate admin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,17 +10,19 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            <!-- Navigation Links -->
+            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('todo.index')" :active="request()->routeIs('todo.index')">
                         {{ __('Todo') }}
                     </x-nav-link>
+                    @can('admin')
                     <x-nav-link :href="route('user.index')" :active="request()->routeIs('user.index')">
                         {{ __('User') }}
-                    </x-nav-link> 
+                    </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
---
+++ screenshot/tugas7/tugas7.md
@@ -0,0 +1,5 @@
+a. Middleware & Gate Admin Not Active
+![alt text](<Screenshot 2025-05-03 152139.png>)
+
+b. Middleware & Gate Admin Active
+![alt text](<Screenshot 2025-05-03 151935.png>)
