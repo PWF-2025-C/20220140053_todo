cbe866733df96617532b2d9e3e2c63bc09674c23
andiniseptia <andiniseptiandri124@gmail.com>
2025-05-24 16:29:11 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/todo', [TodoController::class, 'index']) ->name('todo.index');
     Route::get('todo/create', [TodoController::class, 'create']) ->name('todo.create');
-    Route::get('todo/edit', [TodoController::class, 'edit']) ->name('todo.edit');
+    // Route::get('todo/edit', [TodoController::class, 'edit']) ->name('todo.edit');
 
     //Edit Data – Todo
     Route::get('todo/{todo}/edit', [TodoController::class, 'edit']) ->name('todo.edit');
@@ -53,4 +53,5 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::patch('user/{user}/makeadmin', [UserController::class, 'makeadmin']) ->name('user.makeadmin');
     Route::patch('user/{user}/removeadmin', [UserController::class, 'removeadmin']) ->name('user.removeadmin');
 });
+
 require __DIR__.'/auth.php';
---
+++ screenshot/tugas10/tugas10.md
@@ -0,0 +1,11 @@
+a. Browser Register
+![alt text](<Screenshot (98).png>)
+
+b. Browser Verify Email
+![alt text](<Screenshot (99).png>)
+
+c. Browser Mailtrap 
+![alt text](<Screenshot (100).png>)
+
+d. Browser Dashboard
+![alt text](<Screenshot (101).png>)
